1e01f2a4fb661f908beca3ddeaaab601bd6128b3
Yusuf Xojimurodov <xojimurodovyusuf7@gmail.com>
2025-10-01 16:54:49 +0500

    website no working

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<!-- <script setup>
+<script setup>
 import IconTechnicIssue from './components/icons/IconTechnicIssue.vue'
 </script>
 
 <template>
   <div class="down-wrapper">
     <div class="flex justify-center items-center gap-4">
-      <icon-technic-issue />
+      <IconTechnicIssue />
       <h1 class="!p-0 !m-0">Texnik ishlar olib borilmoqda</h1>
     </div>
     <p>Iltimos, keyinroq yana urinib ko‘ring.</p>
@@ -20,7 +20,7 @@ import IconTechnicIssue from './components/icons/IconTechnicIssue.vue'
   gap: 10px;
   justify-content: center;
   align-items: center;
-  background: #0f172a;
+  background: #FF8C00;
   color: #f8fafc;
   font-family: Arial, sans-serif;
   text-align: center;
@@ -35,10 +35,10 @@ p {
   font-size: 1.2rem;
   opacity: 0.8;
 }
-</style> -->
-
+</style>
+<!-- 
 <script setup></script>
 
 <template>
   <router-view />
-</template>
+</template> -->
